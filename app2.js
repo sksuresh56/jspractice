3d3414037ccf99b2f7be6e247bede00158a1af00
suresh <sksuresh56@gmail.com>
2019-05-12 08:32:02 +0530

    my details

--- app2.js
+++ app2.js
@@ -1,3 +1,21 @@
-var fruits = ['Apple', 'Banana'];
+let name ={
+ firstname :"suresh", lastname :"kumar"}
 
-console.log(fruits.length);
+ console.log(name.firstname);
+
+ let married=false
+
+ console.log(married)
+
+ let address={
+    no:4,street:"ram nagar",currentAddress:true,city:{
+        state:"tn",pincode:6010
+    }
+}
+
+ console.log(address.city.pincode)
+ 
+
+ let fruit=['apple','orange']
+
+ console.log(fruit[0])
